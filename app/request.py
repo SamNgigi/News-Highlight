62444e0e2ada46f3c49ef3e711c41928907c149e
Sam Ngigi <sam.t.ngigi@gmail.com>
2018-01-26 13:28:18 +0300

    Able to fetch appriate data

--- app/request.py
+++ app/request.py
@@ -19,19 +19,28 @@ def get_sources(country, category):
     with urllib.request.urlopen(get_news_url) as url:
         get_news_data = url.read()
         get_news_response = json.loads(get_news_data)
-        print(get_news_response)
+        # print(get_news_response)
         # get_news_response is now a dictionary because of the json.loads()
 
         news_results = None
 
-        # if get_news_response['sources']:
-        #     news_results_list = get_news_response['sources']
-        #     news_results = process_results(news_results_list)
+        if get_news_response['sources']:
+            news_results_list = get_news_response['sources']
+            news_results = process_results(news_results_list)
+
+    return news_results
+
+
+def process_results(source_list):
     """
     We now want to process the dictionary and
     output a list of objects - news_results.
 
-    We will use the process_results() function to do that.
+    We process results will transform our dictionary into a list of objects.
     """
-
-    return news_results
+    # news_results = []
+    for source in source_list:
+        id = source.get('id')
+        print(id)
+        name = source.get('name')
+        print(name)
