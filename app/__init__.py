15cb3f0dd85cdca43c55030cec84fd8abe89da0d
Sam Ngigi <sam.t.ngigi@gmail.com>
2018-01-26 15:44:12 +0300

    Downloaded flask bootstrap

--- app/__init__.py
+++ app/__init__.py
@@ -14,4 +14,7 @@ app = Flask(__name__, instance_relative_config=True)
 app.config.from_object(DevConfig)
 app.config.from_pyfile('config.py')
 
+# Initializing Flask Extension
+bootstrap = Bootstrap(app)
+
 from app import views
