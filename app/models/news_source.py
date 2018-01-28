253284312b0ae22733e36706611343efbdb0996c
Sam Ngigi <sam.t.ngigi@gmail.com>
2018-01-28 10:23:41 +0300

    Source test passed

--- app/models/news_source.py
+++ app/models/news_source.py
@@ -3,10 +3,10 @@ class Source:
     Source class to define news source object
     """
 
-    # def __init__(self, id, name, description, url, category, country):
-    #     self.id = id
-    #     self.name = name
-    #     self.description = description
-    #     self.url = url
-    #     self.category = category
-    #     self.country = country
+    def __init__(self, id, name, description, url, category, country):
+        self.id = id
+        self.name = name
+        self.description = description
+        self.url = url
+        self.category = category
+        self.country = country
