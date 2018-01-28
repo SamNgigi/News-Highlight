8525806eb9750c395a4acb2e1563384b91507ab3
Sam Ngigi <sam.t.ngigi@gmail.com>
2018-01-28 12:26:15 +0300

    Added Procfile and requirements.txt

--- manager.py
+++ manager.py
@@ -8,5 +8,16 @@ app = create_app('development')
 manager = Manager(app)
 manager.add_command('server', Server)
 
+
+@manager.command
+def test():
+    """
+    Runs the unit tests.
+    """
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
+
+
 if __name__ == '__main__':
     manager.run()
