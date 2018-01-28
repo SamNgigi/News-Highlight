06a3c2eddc6d6e5583959ebb262afd7a129aa445
Sam Ngigi <sam.t.ngigi@gmail.com>
2018-01-28 10:41:58 +0300

    Test failed and then passed

--- app/models/news_articles.py
+++ app/models/news_articles.py
@@ -4,7 +4,14 @@ class Articles:
     """
 
     def __init__(self,
-                 id, name, author, title, description, url, urlToImage, publishedAt):
+                 id,
+                 name,
+                 author,
+                 title,
+                 description,
+                 url,
+                 urlToImage,
+                 publishedAt):
         self.id = id
         self.name = name
         self.author = author
